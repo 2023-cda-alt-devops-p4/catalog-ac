880c78e2bbdecf8d70c13022764ac6fa25df7f7a
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-09-19 15:59:48 +0200

    🏗️  add route, styled-components, react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import styled from "styled-components";
+
 function App() {
 	return (
 		<>
-			<h1>Catalogue de diagrammes UML + Merise
-				---------------
-			</h1>
+			<h1>Catalogue de diagrammes UML + Merise ---------------</h1>
 		</>
 	);
 }
+// const  = styled.`     `;
+	
 
 export default App;
